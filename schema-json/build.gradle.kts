e1306c747403a758dfdc322d94b258c7d5c8c43b
Ben Basinski <benbasinski97@gmail.com>
2024-10-26 13:34:22 -0700

    more

--- schema-json/build.gradle.kts
+++ schema-json/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     id("module.publication")
@@ -16,6 +18,41 @@ kotlin {
             dependencies {
                 implementation(libs.kotlin.test)
             }
+        }
+    }
+}
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    signAllPublications()
+
+    coordinates(group.toString(), "schema-json", version.toString())
+
+    pom {
+        name = "Katalyst"
+        description = "Accelerating development with powerful Kotlin libraries"
+        url = "https://github.com/benbasinski/katalyst"
+        inceptionYear = "2024"
+
+        developers {
+            developer {
+                id = "bbasinsk"
+                name = "Ben Basinski"
+            }
+        }
+
+        licenses {
+            license {
+                name = "The Apache License, Version 2.0"
+                url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+            }
+        }
+
+        scm {
+            url = "https://github.com/benbasinski/katalyst"
+            connection = "scm:git:git://github.com/benbasinski/katalyst.git"
+            developerConnection = "scm:git:ssh://github.com/benbasinski/katalyst.git"
         }
     }
 }
