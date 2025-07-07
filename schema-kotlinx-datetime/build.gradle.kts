07c8f502d019f2abbe5fec162f8e379780ad7db5
Ben Basinski <benbasinski97@gmail.com>
2025-07-07 16:09:22 -0700

    bump versions

--- schema-kotlinx-datetime/build.gradle.kts
+++ schema-kotlinx-datetime/build.gradle.kts
@@ -9,6 +9,10 @@ kotlin {
     macosArm64()
     linuxX64()
 
+    compilerOptions {
+        optIn = listOf("kotlin.time.ExperimentalTime")
+    }
+
     sourceSets {
         val commonMain by getting {
             dependencies {
