b3a6c339637c3f8b5914726e1dbc44b6428eef5f
Ben Basinski <benbasinski97@gmail.com>
2025-10-05 13:08:24 -0700

    cleanup

--- http/src/commonMain/kotlin/io/github/bbasinsk/http/HttpEndpointGroup.kt
+++ http/src/commonMain/kotlin/io/github/bbasinsk/http/HttpEndpointGroup.kt
@@ -1,11 +1,10 @@
 package io.github.bbasinsk.http
 
-abstract class HttpEndpointGroup(vararg tags: String) {
-    private val groupTags = tags.toList()
+abstract class HttpEndpointGroup(vararg val tags: String) {
     private val _apis = mutableListOf<Http<*, *, *, *>>()
 
     fun <P, I, E, O> api(definition: Http<P, I, E, O>): Http<P, I, E, O> =
-        definition.tag(*groupTags.toTypedArray()).also { _apis.add(it) }
+        definition.tag(*tags).also { _apis.add(it) }
 
     fun <P, I, E, O> http(build: Http.Companion.() -> Http<P, I, E, O>): Http<P, I, E, O> =
         api(Http.build())
