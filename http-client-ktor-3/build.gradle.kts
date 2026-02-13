7bc16661ffaa0197ad91b49b10e43b05cbf7dc6b
Ben Basinski <benbasinski97@gmail.com>
2026-02-13 14:09:55 -0800

    update

--- http-client-ktor-3/build.gradle.kts
+++ http-client-ktor-3/build.gradle.kts
@@ -1,5 +1,3 @@
-import com.vanniktech.maven.publish.SonatypeHost
-
 plugins {
     id("katalyst.library")
 }
@@ -31,7 +29,7 @@ kotlin {
 }
 
 mavenPublishing {
-    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    publishToMavenCentral()
 
     signAllPublications()
 
